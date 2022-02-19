ed7d75be83e1706ef6f2908a33e71ef74aa3eedc
Rıdvan KÜNTUĞ <ridvankuntug@gmail.com>
2022-02-19 10:55:17 +0300

    Proje dosyası ekle.

---
+++ ProducerApi/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProducerApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ConnectionFactory _factory;
+        private readonly IConnection _connection;
+
+        public HomeController()
+        {
+            _factory = new ConnectionFactory()
+            {
+                Uri = new Uri("https://customer.cloudamqp.com/login")
+            };
+            _connection = _factory.CreateConnection();
+
+        }
+
+
+
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            using var channel = _connection.CreateModel();
+            channel.QueueDeclare("inventiv-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+
+
+                var message = "Hello Inventiv: " + DateTime.Now + "!..";
+                var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+                //Exchange parametresi boş ayarlanırsa, default olarak direct exchange olarak davranır.
+                channel.BasicPublish("", "inventiv-queue", null, body);
+
+            return Ok("Mesaj Gönderldi" + message);
+        }
+    }
+}
